8c2a0919a8a990526318f91f075a3ebf79cd113f
katypaola11 <kpantoja1117@gmail.com>
2026-04-20 01:13:55 -0500

    angular

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,6 +4,9 @@ import { GuiaSpring } from "../pages/GuiaSpring";
 import { GuiaKaty } from "../pages/GuiaKaty";
 import { GuiaReact } from "../pages/Guiareact";
 import GuiaMovil from '../pages/GuiaMovil';
+import { GuiaAngular } from "../pages/angular";
+
+
 
 export const AppRoutes = () => {
   return (
@@ -14,6 +17,7 @@ export const AppRoutes = () => {
         <Route path="/guiaKaty" element={<GuiaKaty />} />
         <Route path="/react-spring" element={<GuiaReact />} />
          <Route path="/guiaMovil" element={<GuiaMovil/>} />
+         <Route path="/guiaAngular" element={<GuiaAngular/>} />
       </Routes>
     </>
   );
